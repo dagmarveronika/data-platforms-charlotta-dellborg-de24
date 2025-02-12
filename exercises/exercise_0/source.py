b982d5e67ee0d322ba7f6ffc5fbfb54b48bf41ff
Charlotta <charlotta.dellborg@hotmail.se>
2025-02-12 16:47:30 +0100

    0 and 1a

--- exercises/exercise_0/source.py
+++ exercises/exercise_0/source.py
@@ -9,3 +9,25 @@ print("\nSource: ", sourcePath, "\nDestination: ", destinationPath)
 #1. Schema validation
 member = {"id": 101, "name": "Erika", "is_active": True, "age": 45}
 
+def ValidateData(aDictionary):
+    errorMessage = "Error:"
+    if type(aDictionary) != dict:
+        errorMessage += "\nData set is not a dictionary"
+
+    if type(aDictionary["id"]) != int:
+        errorMessage += "\nID is not an integer"
+
+    if type(aDictionary["name"]) != str:
+        errorMessage += "\nName is not a string"
+    
+    if type(aDictionary["is_active"]) != bool:
+        errorMessage += "\nis_active is not a boolean"
+    
+    if type(aDictionary["age"]) != int:
+        errorMessage += "\nAge is not an integer"
+
+    if errorMessage == "Error:":
+        return True
+    else:
+        print(errorMessage)
+        return False
