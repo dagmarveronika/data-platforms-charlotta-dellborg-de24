3a360b8a05f162715cbee48a742eb377c6f59dd0
Charlotta <charlotta.dellborg@hotmail.se>
2025-02-12 16:17:24 +0100

    first exercise started

---
+++ exercises/exercise_0/source.py
@@ -0,0 +1,11 @@
+#0. User input for ETL parameters
+sourcePath = input("Please enter the file path for your source file: ")
+destinationPath = input("Please enter the file path for your destination file: ")
+
+print("\nSource: ", sourcePath, "\nDestination: ", destinationPath)
+
+
+
+#1. Schema validation
+member = {"id": 101, "name": "Erika", "is_active": True, "age": 45}
+
